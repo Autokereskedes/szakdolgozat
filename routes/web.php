dac712549027d0394bd155038e39bed2e8869666
gabascott <smskgbr@gmail.com>
2022-02-14 14:24:25 +0100

    hiányzó .env file és vendor mappa

--- routes/web.php
+++ routes/web.php
@@ -14,5 +14,9 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::get('/index.html', function () {
     return view('index');
 });
