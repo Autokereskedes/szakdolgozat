2febe5f13a469f7f90ae426264fd5a7678220104
RolanHun <79194971+RolanHun@users.noreply.github.com>
2022-03-31 19:55:56 +0200

    Rendelt JS (Még 0db rendelt autó esetén való megjelenítés)

--- public/js/rendelt.js
+++ public/js/rendelt.js
@@ -8,11 +8,15 @@ $(function(){
     rendeltAjaxhivas.getAdat(apivegpont+"rendelt"+"?q="+userId, rendelesek , kiir);
     
     function kiir(){
+        if (rendelesek.length>0){
         rendelesek.forEach(function(elem){
             let node = sablonModell.clone().appendTo(szuloModell);
             const obj = new Rendelt(node, elem);
         })
         sablonModell.remove();
+        } else{
+            $(".rendeltAutok").html('<tr><th>Még nem rendeltél autót!</th><tr><td>Kattints felül a Konfigurátor gombra és kezdj hozzá!</td>')
+        }
     }
 
 });
--- public/js/rendeltAdmin.js
+++ public/js/rendeltAdmin.js
@@ -3,7 +3,7 @@ $(function(){
     const rendelesek = [];
     const szuloModell = $(".rendeltAutok");
     const sablonModell = $(".rendeltAuto");
-    let apivegpont = "http://127.0.0.1:8000/api/";
+    let apivegpont = "/api/";
     rendeltAjaxhivas.getAdat(apivegpont+"rendeltA", rendelesek , kiir);
     
     function kiir(){
